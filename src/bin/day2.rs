10b88417b2f9a4d60efd95fdb32c76461d98493c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-12-02 06:14:20 +0100

    day 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,4 +1,3 @@
-
 use aoc_client::{AocClient, AocResult};
 
 fn main() -> AocResult<()> {
@@ -22,11 +21,35 @@ fn main() -> AocResult<()> {
 }
 
 fn part1(file: String) -> String {
-	let res = 1;
+	let res = file.lines().filter(|line| {
+		let list = line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
+		let mut other1 = list.clone();
+		other1.sort();
+		let mut other2 = list.clone();
+		other2.sort();
+		other2.reverse();
+		(list == other1 || list == other2)
+		&& list.windows(2).all(|x| x[0].abs_diff(x[1]) >= 1 && x[0].abs_diff(x[1]) <= 3 )
+	}).count();
 	return res.to_string();
 }
 
 fn part2(file: String) -> String {
-	let res = 1;
+	let res = file.lines().filter(|line| {
+		let list = line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
+		for i in 0..list.len() {
+			let list = list[0..i].iter().chain(list[i+1..list.len()].iter()).cloned().collect::<Vec<_>>();
+			let mut other1 = list.clone();
+			other1.sort();
+			let mut other2 = list.clone();
+			other2.sort();
+			other2.reverse();
+			if (list == other1 || list == other2)
+				&& list.windows(2).all(|x| x[0].abs_diff(x[1]) >= 1 && x[0].abs_diff(x[1]) <= 3 ) {
+					return true;
+			}
+		}
+		false
+	}).count();
 	return res.to_string();
 }
